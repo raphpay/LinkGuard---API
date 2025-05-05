9873f25363e62eb0c894b3dcce5dae16fddfb8fd
raphpay <raphpay.dev@gmail.com>
2025-05-05 14:03:27 +0400

    Feature: Prepare app with mongodb #1

--- Sources/linkguard-api/configure.swift
+++ Sources/linkguard-api/configure.swift
@@ -1,7 +1,13 @@
 import Vapor
+import Fluent
+import FluentMongoDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
+    // Configure MongoDB
+    let dbURL = Environment.get("DATABASE_URL") ?? "mongodb://localhost:27017/linkguard"
+    try app.databases.use(.mongo(connectionString: dbURL), as: .mongo)
+
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
